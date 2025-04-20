a70a7f3bcfa6db95f39c0df6b61cdea3bf67aea7
bradleycfleming <brad10fleming@gmail.com>
2025-04-20 17:59:20 -0500

    Cleaned up imports and variables

--- app/components/chessboard.tsx
+++ app/components/chessboard.tsx
@@ -57,40 +57,4 @@ const Chessboard: React.FC<Props> = (props) => {
   );
 };
 
-const getPiecePosition = (color: string, type: string) => {
-  // map pieces to their position in the sprite
-  const pieceMap: { [key: string]: { [key: string]: string } } = {
-    w: {
-      p: "0% 0%", // White Pawn
-      r: "0% 12.5%", // White Rook
-      n: "0% 25%", // White Knight
-      b: "0% 37.5%", // White Bishop
-      q: "0% 50%", // White Queen
-      k: "0% 62.5%", // White King
-    },
-    b: {
-      p: "12.5% 0%", // Black Pawn
-      r: "12.5% 12.5%", // Black Rook
-      n: "12.5% 25%", // Black Knight
-      b: "12.5% 37.5%", // Black Bishop
-      q: "12.5% 50%", // Black Queen
-      k: "12.5% 62.5%", // Black King
-    },
-  };
-
-  return pieceMap[color]?.[type] || "0% 0%";
-};
-
-// colors
-const WHITE = "w";
-const BLACK = "b";
-
-// pieces
-const PAWN = "p";
-const KNIGHT = "n";
-const BISHOP = "b";
-const ROOK = "r";
-const QUEEN = "q";
-const KING = "k";
-
 export default Chessboard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-import { Chess } from "chess.js";
 import { createClient } from "@supabase/supabase-js";
 import Chessboard from "./components/chessboard";
 
@@ -26,21 +24,6 @@ export default async function Home() {
         <Chessboard fen={puzzle.FEN}></Chessboard>
       </main>
       <footer className="">
-        {/* <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a> */}
       </footer>
     </div>
   );
